cbc00cd41205c5acb329faf8c26d6b9b6e6977dd
Lijo M Loyid <lijomloyid@gmail.com>
2020-07-06 01:35:49 -0700

    skeleton only

---
+++ src/app/material.ts
@@ -0,0 +1,14 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@NgModule({
+    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule ],
+    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule ],
+
+})
+export class MaterialModule{
+
+}
